d7f144119e42b18ac6c4ba5e1d0c8e9d526caabc
GoblinBackwards <22856555+GoblinBackwards@users.noreply.github.com>
2025-04-26 16:17:35 +0100

    Add project files.

---
+++ TimCoreyFullStackChallengeWinforms/Form1.cs
@@ -0,0 +1,10 @@
+namespace TimCoreyFullStackChallengeWinforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
