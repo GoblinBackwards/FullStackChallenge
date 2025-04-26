b071caa419cdde5bab7dc2ec052927b3a96e70fd
GoblinBackwards <22856555+GoblinBackwards@users.noreply.github.com>
2025-04-26 18:54:59 +0100

    Improved error handling and loading message on website

--- timcoreyfullstackchallengewebsite/src/App.tsx
+++ timcoreyfullstackchallengewebsite/src/App.tsx
@@ -3,19 +3,30 @@ import './App.css'
 import { Employee, EmployeeTable } from './components/EmployeeTable'
 
 function App() {
-  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [employees, setEmployees] = useState<Employee[] | null>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
+    setLoading(true);
     fetch('https://localhost:7025/')
     .then(res => res.json())
     .then(data => {
+      setLoading(false);
       setEmployees(data as Employee[]);
       console.log(data);
+    })
+    .catch(() => {
+      setLoading(false);
+      setEmployees(null);
     });
   }, []);
 
-  return (
-    <EmployeeTable employees={employees} />
-  )
+  if (loading) {
+    return <h3>Loading...</h3>
+  } else if (employees == null) {
+    return <h2>Failed to connect to the API.</h2>
+  } else {
+    return <EmployeeTable employees={employees} />
+  }
 }
 
 export default App
